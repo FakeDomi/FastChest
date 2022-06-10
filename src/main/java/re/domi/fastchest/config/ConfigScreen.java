3fb473a3b4a15364152b13d92e035e593bc04bbf
Domi <domi@dev.null>
2022-06-10 20:19:29 +0200

    Updated to 1.19 and fixed the FastChest side of #14

--- src/main/java/re/domi/fastchest/config/ConfigScreen.java
+++ src/main/java/re/domi/fastchest/config/ConfigScreen.java
@@ -1,17 +1,16 @@
 package re.domi.fastchest.config;
 
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.ScreenTexts;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.LiteralText;
+import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
 
 public class ConfigScreen extends Screen
 {
-    private static final Text SCREEN_TITLE = new LiteralText("FastChest config");
-    private static final Text SIMPLIFIED_ON = new LiteralText("Simplified chest: ON");
-    private static final Text SIMPLIFIED_OFF = new LiteralText("Simplified chest: OFF");
+    private static final Text SCREEN_TITLE = Text.literal("FastChest config");
+    private static final Text SIMPLIFIED_ON = Text.literal("Simplified chest: ON");
+    private static final Text SIMPLIFIED_OFF = Text.literal("Simplified chest: OFF");
 
     private final Screen parent;
 
@@ -49,7 +48,7 @@ public class ConfigScreen extends Screen
     }
 
     @Override
-    public void onClose()
+    public void close()
     {
         if (this.client != null)
         {
@@ -61,7 +60,7 @@ public class ConfigScreen extends Screen
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta)
     {
         this.renderBackground(matrices);
-        drawCenteredText(matrices, this.textRenderer, this.title.asString(), this.width / 2, 5, 0xFFFFFF);
+        drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 5, 0xFFFFFF);
         super.render(matrices, mouseX, mouseY, delta);
     }
 }
