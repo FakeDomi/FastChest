a1d9db5f7c9c3ebe251810e3537ecb1888e9a339
Domi <domi@dev.null>
2025-12-10 01:07:13 +0100

    1.21.9

--- src/main/java/re/domi/fastchest/mixin/BlockEntityRenderManagerMixin.java
+++ src/main/java/re/domi/fastchest/mixin/BlockEntityRenderManagerMixin.java
@@ -3,19 +3,20 @@ package re.domi.fastchest.mixin;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.block.entity.EnderChestBlockEntity;
-import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
+import net.minecraft.client.render.block.entity.BlockEntityRenderManager;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.block.entity.state.BlockEntityRenderState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import re.domi.fastchest.config.Config;
 
-@Mixin(BlockEntityRenderDispatcher.class)
-public class BlockEntityRenderDispatcherMixin
+@Mixin(BlockEntityRenderManager.class)
+public class BlockEntityRenderManagerMixin
 {
     @Inject(method = "get", at = @At("HEAD"), cancellable = true)
-    private <E extends BlockEntity> void removeBlockEntityRenderer(E blockEntity, CallbackInfoReturnable<BlockEntityRenderer<E>> cir)
+    private <E extends BlockEntity, S extends BlockEntityRenderState> void removeBlockEntityRenderer(E blockEntity, CallbackInfoReturnable<BlockEntityRenderer<E, S>> cir)
     {
         if (Config.simplifiedChest)
         {
